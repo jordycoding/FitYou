f901ff514fa8dd82877bdd2978e2168e61748d07
Jordy Alkema <6128820+jordycoding@users.noreply.github.com>
2023-09-17 18:45:28 +0200

    Added navbar

--- app/src/main/java/com/alkema/fityou/MainActivity.kt
+++ app/src/main/java/com/alkema/fityou/MainActivity.kt
@@ -1,15 +1,38 @@
 package com.alkema.fityou
 
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CalendarMonth
+import androidx.compose.material.icons.filled.FitnessCenter
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.outlined.CalendarMonth
+import androidx.compose.material.icons.outlined.FitnessCenter
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.alkema.fityou.ui.theme.FitYouTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -22,26 +45,79 @@ class MainActivity : ComponentActivity() {
             FitYouTheme {
                 val navController = rememberNavController()
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainView()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
+sealed class Screen(
+    val route: String,
+    @StringRes val resourceId: Int,
+    val icon: ImageVector,
+    val activeIcon: ImageVector
+) {
+    object Home : Screen("home", R.string.home, Icons.Outlined.Home, Icons.Filled.Home)
+    object Calendar : Screen(
+        "calendar",
+        R.string.calendar,
+        Icons.Outlined.CalendarMonth,
+        Icons.Filled.CalendarMonth
+    )
+
+    object Exercises : Screen(
+        "exercises",
+        R.string.exercises,
+        Icons.Outlined.FitnessCenter,
+        Icons.Filled.FitnessCenter
     )
 }
 
-@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GreetingPreview() {
-    FitYouTheme {
-        Greeting("Android")
+fun MainView(modifier: Modifier = Modifier) {
+    val items = listOf(Screen.Home, Screen.Calendar, Screen.Exercises)
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = {
+            NavigationBar {
+                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                val currentDestination = navBackStackEntry?.destination
+                items.forEach { screen ->
+                    val active =
+                        currentDestination?.hierarchy?.any { it.route == screen.route } == true
+                    val icon = if (active) screen.activeIcon else screen.icon
+                    NavigationBarItem(
+                        icon = { Icon((icon), contentDescription = null) },
+                        label = { Text(text = stringResource(id = screen.resourceId)) },
+                        selected = active,
+                        onClick = {
+                            navController.navigate(screen.route) {
+                                popUpTo(navController.graph.findStartDestination().id) {
+                                    saveState = true
+                                }
+                                launchSingleTop = true
+                                restoreState = true
+                            }
+                        }
+                    )
+                }
+            }
+        }
+    ) { innerPadding ->
+        NavHost(
+            navController,
+            startDestination = Screen.Home.route,
+            modifier.padding(innerPadding)
+        ) {
+            composable(Screen.Home.route) { Text("Home") }
+            composable(Screen.Calendar.route) { Text("calendar") }
+            composable(Screen.Exercises.route) { Text("exercises") }
+        }
     }
 }
